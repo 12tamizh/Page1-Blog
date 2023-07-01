302ef2ffb35a4a027a805f36f8bc43d7717abde9
tamizh <tamizh.upworks@gmail.com>
2023-07-01 14:25:15 +0530

    Initial Commit

--- Sqlmodels.py
+++ Sqlmodels.py
@@ -11,7 +11,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'log_in'
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("APP_SECRET_KEY")
 db = SQLAlchemy(app)
@@ -50,5 +50,5 @@ class Comments(db.Model):
     comment = db.Column(db.String(500), nullable=False)
 
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
